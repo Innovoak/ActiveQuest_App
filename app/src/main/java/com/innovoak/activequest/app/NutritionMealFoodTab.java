d3f028d7c00da1aaf9371a12d61bbd2fcfde57c3
shamanthi <shamanthi2025@gmail.com>
2023-08-17 10:04:30 -0400

    2 Nutrition pages, one friend individual profile, and leader board.

---
+++ app/src/main/java/com/innovoak/activequest/app/NutritionMealFoodTab.java
@@ -0,0 +1,20 @@
+package com.innovoak.activequest.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NutritionMealFoodTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrition_meal_food_tab);
+
+    }
+}
---
+++ app/src/main/java/com/innovoak/activequest/app/Nutrition_Food_Detail_Tab.java
@@ -0,0 +1,13 @@
+package com.innovoak.activequest.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nutrition_Food_Detail_Tab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrition_food_detail_tab);
+    }
+}
